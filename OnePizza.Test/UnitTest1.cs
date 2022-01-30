d03481a6a0e206b433af07e3794082dfe7d69c47
stormholder <dantedmc78@gmail.com>
2022-01-30 14:43:02 +0300

    Initial structure commit

---
+++ OnePizza.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace OnePizza.Test
+{
+    public class SampleTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
