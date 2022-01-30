8d3778dbeb844a4655151f4351f54681720d62b2
stormholder <dantedmc78@gmail.com>
2022-01-30 18:24:49 +0300

    Added I/O for main loop. Updated publish properties

--- OnePizza/Program.cs
+++ OnePizza/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+if (args.Length < 2)
+{
+	Console.WriteLine("2 arguments required: Input file, output file");
+	return;
+}
+
+try
+{
+    string file = "";
+	using (var sr = new StreamReader(args[0]))
+    {
+        file = sr.ReadToEnd();
+    }
+    var pizzaBuilder = new OnePizza.PizzaBuilder();
+    pizzaBuilder.ParseInput(file);
+    var ingredients = pizzaBuilder.MixIngredients();
+    string? output = pizzaBuilder.ProducePizza(ingredients);
+
+    using (var sw = new StreamWriter(args[1]))
+    {
+        sw.WriteLine(output);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(ex.Message);
+    return;
+}
